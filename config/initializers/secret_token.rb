19e2b5eefc16723a6b61fc901eaa11f539cc650a
Laura <girl@alicetragedy.org>
2013-03-04 11:20:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7cf51d138f560d8676322a07b81f65d4db3a991b3656a8cb11f7d072cb5fb87b08f328f4d7eaa18f053bb2da6dedaf5805602c33d2f4d95c68c8eb0c1f594a72'
